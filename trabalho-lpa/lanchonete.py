2cffe478f7eaf7ca2285dae82e27a0c9175ed306
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-06-17 19:40:42 -0300

    lanchonete

---
+++ trabalho-lpa/lanchonete.py
@@ -0,0 +1,63 @@
+print('._____________________________________________.')
+print('|             Lanchonete do Yuri              |')
+print('|_________________CARDÁPIO____________________|')
+print('| Código |       Descrição       | Valor (R$) |')
+print('|  100   |    Cachorro-Quente    |    9.00    |')
+print('|  101   | Cachorro-Quente Duplo |   11.00    |')
+print('|  102   |        X-Egg          |   12.00    |')
+print('|  103   |        X-Salada       |   12.00    |')
+print('|  104   |        X-Bacon        |   14.00    |')
+print('|  105   |        X-Tudo         |   17.00    |')
+print('|  200   |   Refrigerante Lata   |    5.00    |')
+print('|  201   |      Chá Gelado       |    4.00    |')
+print('¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨')
+
+acumulador=0 #acumulador de pedidos
+while True:
+
+  codigo = input('Digite o código do produto desejado: ')
+  if codigo !='100' and codigo !='101' and codigo !='102' and codigo !='103' and codigo !='104' and codigo !='105' and codigo !='200' and codigo !='201':
+    print('Opção inválida. Por gentileza digite um código existente!')
+    continue #verifica se o código digitado é existente, se não repete o processo
+
+  if codigo == '100':
+    print('Você escolheu um Cachorro-Quente no valor de R$9.00')
+    acumulador=acumulador+9
+
+  elif codigo == '101':
+    print('Você escolheu um Cachorro-Quente Duplo no valor de R$11.00')
+    acumulador=acumulador+11
+
+  elif codigo == '102':
+    print('Você escolheu um X-Egg no valor de R$12.00')
+    acumulador=acumulador+12
+
+  elif codigo == '103':
+    print('Você escolheu um X-Salada no valor de R$12.00')
+    acumulador=acumulador+12
+
+  elif codigo == '104':
+    print('Você escolheu um X-Bacon no valor de R$14.00')
+    acumulador=acumulador+14
+
+  elif codigo == '105':
+    print('Você escolheu um X-Tudo no valor de R$17.00')
+    acumulador=acumulador+17
+
+  elif codigo == '200':
+    print('Você escolheu um Refrigerante Lata no valor de R$5.00')
+    acumulador=acumulador+5
+
+  elif codigo == '201':
+    print('Você escolheu um Chá Gelado no valor de R$4.00')
+    acumulador=acumulador+4
+
+  pedir_mais = input('Deseja pedir mais alguma coisa? (sim/não) ')
+  if pedir_mais == 'sim':
+    print('Faça mais um pedido!')
+    continue #para fazer mais pedidos o cliente digita 'sim' e para finalizar digita 'não'
+
+  else: #programa finaliza com o valor total dos pedidos feito pelo cliente
+    print('Valor total a ser pago: R${:.2f}'.format(acumulador))
+    print('PEDIDO FINALIZADO, volte sempre!')
+    break
