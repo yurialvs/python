99e51a63af1176a8fa9f4051d6053a4b962a6a27
Yuri <lops.yuri@gmail.com>
2024-05-26 12:12:46 -0300

    variaveis

---
+++ curso-python/variaveis.py
@@ -0,0 +1,30 @@
+nome = "Yuri Alves"
+print(nome)
+
+empresa = "NASA"
+print(empresa)
+
+qtde_funcionarios = 500 #tipo int
+print(qtde_funcionarios)
+
+mediaMensalidade = 856.51 #tipo float
+#Gera o número do tipo float 856.51
+print(float(mediaMensalidade))
+
+#Gera o número do tipo int 856
+print(int(mediaMensalidade))
+
+#Concatenação com operador +
+print(nome + " trabalha na empresa " + empresa)
+
+#Concatenação com virgula
+print("Possui", qtde_funcionarios, "funcionários")
+
+#Utilizando input e convertendo
+idade = input("Qual é a sua idade? ")
+
+# Converte a string para um número inteiro
+idade_inteira = int(idade)
+
+# Exibe a idade como um número inteiro
+print(idade_inteira)
