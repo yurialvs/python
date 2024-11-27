e5a65dfd29b9d1bacbfbec5748a5bacd84c1ed0d
Yuri <lops.yuri@gmail.com>
2024-11-27 10:48:16 -0300

    update dicionarios

---
+++ curso-python/aula_dicionarios/funcoes.py
@@ -0,0 +1,54 @@
+# Função - Opções de navegação
+# \n quebra linha
+# + faz concatenação
+def perguntar():
+    resposta = input('<I> - Para Inserir um usuário \n' +
+                '<P> - Para Pesquisar um usuário \n' +
+                '<E> - Para Excluir um usuário \n' +
+                '<L> - Para Listar um usuário \n' +
+                'O que deseja realizar? ').upper()
+    return resposta
+
+# Função - Inserir usuário
+def inserir(dicionario):
+    dicionario[input('Digite o login: ').upper()] = [input('Digite o nome: ').upper(),
+        input('Digite a última data de acesso: '),
+        input('Qual a última estação acessada: ').upper()]
+    
+# Função - Pesquisar usuário
+def pesquisar(dicionario, chave):
+    lista = dicionario.get(chave)
+    if lista != None:
+        print('Nome...........: ' + lista[0])
+        print('Último acesso..: ' + lista[1])
+        print('Última estação.: ' + lista[2])
+
+# Função - Excluir usuário
+def excluir(dicionario, chave):
+    if dicionario.get(chave) != None:
+        del dicionario.get[chave]
+    print('Usuário excluído!')
+
+# Função - Listar usários
+def listar(dicionario):
+    for chave, valor in dicionario.items():
+        print('Usuário............')
+        print('Login...: ', chave)
+        print('Dados...: ', valor)
+
+# ---------------------------------------------------------------------- #
+# MÉTODOS ADICIONAIS PARA DICIONÁRIOS
+
+# items()
+# Resposável por retornar em forma de lista os elementos do dicionário.
+
+# values()
+# Retorna somente os dados, descartando as chaves.
+
+# keys()
+# Retorna soementa as chaves.
+
+# has_keys()
+# Retorna se a chave existe ou não dentro do dicionário. Ture(1) ou False(0)
+
+#
--- curso-python/aula_dicionarios/users.py
+++ curso-python/aula_dicionarios/users.py
@@ -1,22 +1,15 @@
+from funcoes import *
 usuarios = {}
-
-# \n quebra linha
-# + faz concatenação
-opcao = input('<I> - Para Inserir um usuário \n' +
-              '<P> - Para Pesquisar um usuário \n' +
-              '<E> - Para Excluir um usuário \n' +
-              '<L> - Para Listar um usuário \n' +
-              'O que deseja realizar? ').upper()
+opcao = perguntar()
 
 while opcao == 'I' or opcao == 'P' or opcao == 'E' or opcao == 'L':
     if opcao == 'I':
-        chave = input('Digite o login: ').upper()
-        nome = input('Digite o nome: ').upper()
-        data = input('Digite a última data de acesso: ')
-        estacao = input('Qual a útima estação acessada: ').upper()
-        usuarios[chave] = [nome, data, estacao]
-    opcao = input('<I> - Para Inserir um usuário \n' +
-              '<P> - Para Pesquisar um usuário \n' +
-              '<E> - Para Excluir um usuário \n' +
-              '<L> - Para Listar um usuário \n' +
-              'O que deseja realizar? ').upper()
+        inserir(usuarios)
+    if opcao == 'P':
+        pesquisar(usuarios, input('Qual login deseja pesquisar? '))
+    if opcao == 'E':
+        excluir(usuarios, input('Qual login deseja excluir? '))
+    if opcao == 'L':
+        listar(usuarios)
+    opcao = perguntar()
+    
